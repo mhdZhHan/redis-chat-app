6d7ccfaa3c3449b32e0b1a96db2157adc9d1ba59
Mohammed <dev.mohammedsh@gmail.com>
2024-08-30 10:48:39 +0530

    ⭐ PreferencesTab, dark/light mode & sound management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import PreferencesTab from "@/components/PreferencesTab"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home Page
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center md:px-24 p-4 py-32 gap-4">
+			<PreferencesTab />
+			{/* doted bg */}
+			<div
+				className="absolute top-0 z-[-2] h-screen w-screen dark:bg-[#000000] dark:bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] 
+				dark:bg-[size:20px_20px] bg-[#ffffff] bg-[radial-gradient(#00000033_1px,#ffffff_1px)] bg-[size:20px_20px]"
+				aria-hidden="true"
+			/>
+		</main>
+	)
 }
