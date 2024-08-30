230cc216a4300625a98d25bf1807f36437f475f5
Mohammed <dev.mohammedsh@gmail.com>
2024-08-30 08:40:41 +0530

    🏝️ Auth page UI

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+
+import AuthButtons from "@/components/AuthButtons"
+
+const page = () => {
+	return (
+		<div className="flex h-screen w-full">
+			<div className="flex-1 flex overflow-hidden items-center justify-center relative dark:bg-[#651c2b55] bg-[#651c2b]">
+				<img
+					src="/redis-logo.svg"
+					alt="redis-logo"
+					className="absolute -left-1/4 opacity-25 -bottom-52 lg:scale-125 xl:scale-100 scale-[2] pointer-events-none select-none -z-1"
+				/>
+
+				<div className="flex flex-col gap-2 px-4 xl:ml-40 text-center md:text-start font-semibold">
+					<Image
+						src={"/logo.png"}
+						alt="RedisStash logo"
+						width={763}
+						height={173}
+						className="mt-20 w-[420px] z-0 pointer-events-none select-none"
+					/>
+
+					<p className="text-2xl md:text-3xl text-balance z-10">
+						The{" "}
+						<span className="bg-red-500 px-2 font-bold text-white">
+							ULTIMATE
+						</span>
+						Chat App
+					</p>
+
+					<p className="text-2xl md:text-3xl text-balance z-10 mb-32">
+						You
+						<span className="bg-green-500/90 px-2 font-bold text-white">
+							NEED TO
+						</span>
+						Build
+					</p>
+
+					<AuthButtons />
+				</div>
+			</div>
+
+			<div className="flex-1 relative overflow-hidden justify-center items-center hidden md:flex">
+				<Image
+					src={"/hero-right.png"}
+					alt="Hero Image"
+					fill
+					className="object-cover dark:opacity-60 opacity-90 pointer-events-none select-none h-full"
+				/>
+			</div>
+		</div>
+	)
+}
+export default page
